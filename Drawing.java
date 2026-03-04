947a1b8c9b451939b4c15ed26d329dfe2c51aec3
Viljo Savolainen <viljo.savolainen2001@gmail.com>
2026-03-04 23:19:07 +0200

    feat(drawing): added welcome message

--- Drawing.java
+++ Drawing.java
@@ -4,6 +4,8 @@ public class Drawing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welcome to ASCII drawing app!");
+
         scanner.close();
     }    
 }
