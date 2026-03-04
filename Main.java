12a7efaac9ff728f40f96e9ceb21c9b5ed0ae9b7
Kare Palokallio <kare.palokallio@tuni.fi>
2026-03-04 22:43:59 +0200

    docs(rps) add javadoc for Main class

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
+/**
+ * Main class serves as the entry point for the application.
+ *
+ * It provides a menu for users to select between difrfent functionalities:
+ * Calculator, Rock-Paper-Scissors game, Memory Game.
+ *
+ * The user can exit the application by selecting the quit option.
+ */
+
 public class Main {
 
     public static void main(String[] args) {
