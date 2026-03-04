3cee7525ad2f40b5f8d19fa29791cfccc50a9210
Viljo Savolainen <viljo.savolainen2001@gmail.com>
2026-03-04 23:37:57 +0200

    feat(drawShape) create for-loops

--- Drawing.java
+++ Drawing.java
@@ -17,6 +17,10 @@ public class Drawing {
 
     }
     public static void drawShape(int width, int height) {
-
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                
+            }
+        }
     } 
 }
