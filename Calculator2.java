15a9ae49caf4d4862294b472f725ab1717e838ae
Viljo Savolainen <viljo.savolainen2001@gmail.com>
2026-03-05 16:12:44 +0200

    feat(divide): add method to calculate division

--- Calculator2.java
+++ Calculator2.java
@@ -5,4 +5,7 @@ public class Calculator2 {
     public static double times(double m, double n) {
         return m * n;
     }
+    public static double divide(double m, double n) {
+        return m / n;
+    }
 }
