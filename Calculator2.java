fe48f634425b1796da7541e365972a08d9d4e737
Viljo Savolainen <viljo.savolainen2001@gmail.com>
2026-03-05 16:12:08 +0200

    feat(sum): add method to calculate times

--- Calculator2.java
+++ Calculator2.java
@@ -2,4 +2,7 @@ public class Calculator2 {
     public static double sum(double m, double n) {
         return m + n;
     }
+    public static double times(double m, double n) {
+        return m * n;
+    }
 }
